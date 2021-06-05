469bd86c9916ea0dfe61308a9a85937508c7d0ad
Santi López Bayo <slopez@MacBook-Pro-de-Santi.local>
2021-06-05 15:21:12 +0200

    remove dist from repository

--- server/dist/modules/properties/domain/PropertyId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PropertyId = void 0;
-const Uuid_1 = require("~/shared/domain/valueObject/Uuid");
-class PropertyId extends Uuid_1.Uuid {
-}
-exports.PropertyId = PropertyId;
-//# sourceMappingURL=PropertyId.js.map
