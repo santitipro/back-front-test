fb3f232593ab4de7d70d8ecb899c716e2813de03
Santi López Bayo <slopez@MacBook-Pro-de-Santi.local>
2021-06-08 13:05:23 +0200

    ignore server dist tests

--- server/jest.config.js
+++ server/jest.config.js
@@ -4,5 +4,6 @@ module.exports = {
   setupFiles: ['./tests/setup/setupTests.ts'],
   moduleNameMapper: {
     '~/(.*)': '<rootDir>/src/$1'
-  }
+  },
+  modulePathIgnorePatterns: ['<rootDir>/dist/']
 }
