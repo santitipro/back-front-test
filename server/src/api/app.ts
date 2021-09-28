d8f07641ae6778ab40bf3e73043eb6e63a954f47
Santi López Bayo <slopez@MacBook-Pro-de-Santi.local>
2021-09-28 20:44:17 +0200

    fix app use cors

--- server/src/api/app.ts
+++ server/src/api/app.ts
@@ -7,7 +7,7 @@ const app: Application = express()
 
 app.set('port', process.env.PORT || 3000)
 
-app.use(cors)
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
